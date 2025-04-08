a68c4df2e6898ace140dada1d5067cdc8391039e
MehulDhingra <dhingramehul24@gmail.com>
2025-04-08 17:25:38 +0530

    renamed files

--- store-api/starter/middleware/error-handler.js
+++
@@ -1,6 +0,0 @@
-const errorHandlerMiddleware = async (err, req, res, next) => {
-    console.log(error);
-    res.status(500).json({ msg: "Something went wrong....." })
-}
-
-module.exports = errorHandlerMiddleware
--- store-api/starter/middleware/not-found.js
+++
@@ -1,5 +0,0 @@
-const notFound = async (req, res, next, err) => {
-    console.log(err);
-    res.status(400).json({ msg: "bad request" });
-}
-module.exports = notFound;
